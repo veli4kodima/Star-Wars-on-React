ef93910a09edd94cc36a6c8d671d33d99e378067
veli4kodima <veli4kodima@gmail.com>
2021-04-05 21:28:14 +0300

    API, FETCH

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,10 @@
-import cn from 'classnames';
 import styles from './App.module.css';
+
+import { getApiResource } from '../../utils/network';
 
 const App = () => {
   return(
-    <h1 className={cn(styles.header, styles.text)}>Hello</h1>
+    <h1 className={styles.header}>Hello</h1>
   )
 }
 
